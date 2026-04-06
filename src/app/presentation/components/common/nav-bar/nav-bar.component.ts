fde1bf32fa02e664d3cb0d174e1428a9f9dbab8e
Julian Homez <delivery.consultant12@advancedtbs.com>
2026-04-06 15:38:44 -0500

    julian.homez 04 de Abril de 2026 t 3:38 PM. Cambio de nombre en nav bar, Tablero Proyectos -> Tablero Contratos

--- src/app/presentation/components/common/nav-bar/nav-bar.component.ts
+++ src/app/presentation/components/common/nav-bar/nav-bar.component.ts
@@ -1,96 +1,63 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 
-@Component ({
-    
-    selector: 'nav-bar-component',
-    
-    standalone: true,
-    
-    imports: [RouterLink, RouterLinkActive, CommonModule],
-    
-    templateUrl: './nav-bar.component.html',
-    
-    styleUrls: ['./nav-bar.component.css']
-    
+@Component({
+  selector: 'nav-bar-component',
+
+  standalone: true,
+
+  imports: [RouterLink, RouterLinkActive, CommonModule],
+
+  templateUrl: './nav-bar.component.html',
+
+  styleUrls: ['./nav-bar.component.css'],
 })
-
 export class NavBarComponent implements OnInit, OnDestroy {
-  
-  loggedUser: string =  '';
-  
-  
+  loggedUser: string = '';
+
   ngOnDestroy(): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
   ngOnInit(): void {
-    
-    
     this.loadUserName();
-    
   }
-  
-  
-  
+
   loadUserName(): void {
-        
-        if (typeof window === 'undefined') {
-            
-            return;
-            
-        }
-        
-        const authUserRaw = localStorage.getItem('auth_user');
-        
-        if ( !authUserRaw ) {
-            
-            console.warn('No auth user in localstorage');
-            
-            return;
-        }
-        
-        
-        try {
-            
-            const authUser = JSON.parse(authUserRaw);
-            
-            this.loggedUser = authUser.userName ?? '';
-            
-        } catch (error) {
-            
-            console.error('Error parsing auth user', error);
-            
-        }
-        
+    if (typeof window === 'undefined') {
+      return;
     }
-  
-  
-  
-  
-  
-  openMenus: { [key: string]: boolean } = {
-    
-    home: false,
-    
-    proyectos: false,
-    
-    rh: false,
-    
-    logistics: false
-    
-  };
-  
-  toggleSubmenu(menu: string): void {
-    
-    this.openMenus[menu] = !this.openMenus[menu];
-    
+
+    const authUserRaw = localStorage.getItem('auth_user');
+
+    if (!authUserRaw) {
+      console.warn('No auth user in localstorage');
+
+      return;
+    }
+
+    try {
+      const authUser = JSON.parse(authUserRaw);
+
+      this.loggedUser = authUser.userName ?? '';
+    } catch (error) {
+      console.error('Error parsing auth user', error);
+    }
   }
-  
-  
-  
-  
-  
+
+  openMenus: { [key: string]: boolean } = {
+    home: false,
+
+    proyectos: false,
+
+    rh: false,
+
+    logistics: false,
+  };
+
+  toggleSubmenu(menu: string): void {
+    this.openMenus[menu] = !this.openMenus[menu];
+  }
 }
