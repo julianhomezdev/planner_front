3ce038ead5c83d12cca485ab7cc211d701863532
Julian Homez <delivery.consultant12@advancedtbs.com>
2026-04-07 14:39:59 -0500

    julian.homez 07 de Abril de 2026 t 3:39 PM. deploy yml

--- src/app/environments/development.environment.ts
+++ src/app/environments/development.environment.ts
@@ -1,10 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: '/api',
+  apiUrl: 'http://localhost:5271/api',
   apiTimeout: 30000,
   version: '1.0.0-dev',
   enableDebug: true,
 };
+
+// En despliegue dejar /api
 
 //PROD
 //192.168.1.78:5271 //'http://localhost:5271/api'
